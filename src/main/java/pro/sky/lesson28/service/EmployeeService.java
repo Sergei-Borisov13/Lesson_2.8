00fe8119d0737f9d949f234a719980430c24937a
Sergei Borisov <bmwth2311@gmail.com>
2023-07-07 22:53:35 +0300

    Initial commit

---
+++ src/main/java/pro/sky/lesson28/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson28.service;
+
+import pro.sky.lesson28.model.Employee;
+
+import java.util.Collection;
+
+public interface EmployeeService {
+    Employee add(String firstName, String lastName);
+    Employee remove(String firstName, String lastName);
+    Employee find(String firstName, String lastName);
+
+    Collection<Employee> findAll();
+}
